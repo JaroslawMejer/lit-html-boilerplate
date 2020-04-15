4cb73e1ef8f4675fb6ce9afa2ab26645f64b6fdf
Jaroslaw Mejer <jaroslaw.mejer@gmail.com>
2020-04-15 18:23:12 +0200

    Setup eslint and prettier

--- src/js/HelloWorld.js
+++ src/js/HelloWorld.js
@@ -1,3 +1,20 @@
-export const HelloWorld = () => {
-  return 'Hello, World!'
-}
+import { html, render } from "lit-html";
+
+const people = {
+  name: "Jaroslaw",
+  age: 28,
+  job: "Front End Developer",
+  cssClass: "box worker",
+};
+
+const LitTest = () => {
+  const template = (person) => html`
+    <div class=${person.cssClass}>
+      <h2>Hello! My name is ${person.name}</h2>
+      <p>I am ${person.age} and I work as a ${person.job}</p>
+    </div>
+  `;
+
+  render(template(people), document.body);
+};
+export default LitTest;
